841d3c971b41fd597896bbb7cf9247f9f31d1e1c
bob3457 <czhai1235@gmail.com>
2026-02-10 14:02:36 -0500

    added another test to cipherTest and the cipher documentation

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -34,4 +34,10 @@ class CipherTest {
         String result = cipher.decipher("");
         assertEquals("", result);
     }
+
+    @Test
+    void test4_large_input(){
+        String result = cipher.decipher("bcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890a");
+        assertEquals("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", result);
+    }
 }
